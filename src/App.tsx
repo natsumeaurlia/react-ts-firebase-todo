286faa86569fcac017923067dc2717fdfb3989fd
natsume0718 <moto718program@gmail.com>
2021-09-18 13:47:41 +0900

    effecthooksでデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { db } from './firebase';
 
 const App: React.FC = () => {
-  const [tasks, setTasks] = useState({ id: '', title: '', done: false });
-  return <div className='App'></div>;
+  const [tasks, setTasks] = useState([{ id: '', title: '', done: false }]);
+  useEffect(() => {
+    const unSubscribe = db.collection('tasks').onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().title,
+          done: doc.data().done,
+        }))
+      );
+    });
+    return () => unSubscribe();
+  }, []);
+  return (
+    <div className='App'>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.title}</h3>
+      ))}
+    </div>
+  );
 };
 
 export default App;
